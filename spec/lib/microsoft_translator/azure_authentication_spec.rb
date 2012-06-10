802ad11912357a59d6564ef5d22945b3c5b0d9a4
LupineDev <lupinedev@gmail.com>
2012-06-10 06:56:11 -1000

    moved into request stubs into helper

--- spec/lib/microsoft_translator/azure_authentication_spec.rb
+++ spec/lib/microsoft_translator/azure_authentication_spec.rb
@@ -1,19 +1,6 @@
 require "spec_helper"
 
 describe MicrosoftTranslator::AzureAuthentication do
-  def auth_params(id, secret)
-    {
-      "client_id" => id,
-      "client_secret" => secret,
-      "scope" => URI.encode(MicrosoftTranslator::AzureAuthentication::API_SCOPE),
-      "grant_type" => MicrosoftTranslator::AzureAuthentication::GRANT_TYPE
-    }
-  end
-  def stub_auth_request 
-    response = stub("response", :body => {"access_token"=>"stubbed_token", "token_type"=>"http://schemas.xmlsoap.org/ws/2009/11/swt-token-profile-1.0", "expires_in"=>"599", "scope"=>"http://api.microsofttranslator.com"}.to_json)
-
-    RestClient.stub(:post).and_return(response)
-  end
 
   after :all do
     Timecop.return
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'bundler/setup'
 require 'microsoft_translator'
 require 'timecop' 
 
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
 RSpec.configure do |config|
   # some (optional) config here
 end
