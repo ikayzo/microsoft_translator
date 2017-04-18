441ac946c0452a1c965591f7d67849663a1d9e33
Risa Harada <risa.harada@kidsstar.co.jp>
2017-04-18 13:51:35 +0900

    :beetle: Fix argument to initialize a Client
    Because we dont use it anymore

--- lib/microsoft_translator/client.rb
+++ lib/microsoft_translator/client.rb
@@ -1,12 +1,10 @@
 module MicrosoftTranslator
   class Client
 
-    def initialize(client_id, client_secret)
-      @client_id = client_id
-      raise "client_id must be a string" unless @client_id.is_a?(String)
-      @client_secret = client_secret
-      raise "client_secret must be a string" unless @client_secret.is_a?(String)
-      @auth = MicrosoftTranslator::AzureAuthentication.new(client_id, client_secret)
+    def initialize(subscription_id)
+      @subscription_id = subscription_id
+      raise "subscription_id must be a string" unless @subscription_id.is_a?(String)
+      @auth = MicrosoftTranslator::AzureAuthentication.new(subscription_id)
     end
 
     def translate(text, from_lang, to_lang, content_type)
